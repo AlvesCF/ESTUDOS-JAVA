75b35d63c1ed95162e77372cab382c80a535e532
FernandaAC <73139749+AlvesCF@users.noreply.github.com>
2025-08-12 16:33:48 -0300

    Add files via upload

---
+++ MeuNomeIdade.java
@@ -0,0 +1,7 @@
+public class MeuNomeIdade {
+    public static void main(String[] args) {
+        String nome = "Fer";
+        int idade = 26;
+        System.out.println("Meu nome é " + nome + " e tenho " + idade + " anos.");
+    }
+}
